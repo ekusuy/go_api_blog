f274549d14cd71e54cb0659698e0c78ff7dd25db
ekusuy <ekusuy.omi@gmail.com>
2022-10-04 14:17:01 +0900

    handlersをmuxを使用するように書き換え

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -6,49 +6,25 @@ import (
 )
 
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Hello")
-	} else {
-		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Hello\n")
 }
 
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		io.WriteString(w, "Posting Article")
-	} else {
-		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Posting Article")
 }
 
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Article List")
-	} else {
-		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Article List")
 }
 
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Article Detail")
-	} else {
-		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Article Detail")
 }
 
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		io.WriteString(w, "Posting Nice")
-	} else {
-		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Posting Nice")
 }
 
 func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		io.WriteString(w, "Posting Comment")
-	} else {
-		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Posting Comment")
 }
