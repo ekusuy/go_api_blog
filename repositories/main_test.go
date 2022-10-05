0b64b8a2ff478eec73fb44801c6beb683eba001f
ekusuy <ekusuy.omi@gmail.com>
2022-10-05 23:37:50 +0900

    テストケース追加

--- repositories/main_test.go
+++ repositories/main_test.go
@@ -5,7 +5,15 @@ import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"os"
+	"os/exec"
 	"testing"
+)
+
+var (
+	dbUser     = "docker"
+	dbPassword = "docker"
+	dbDatabase = "sampledb"
+	dbConn     = fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
 )
 
 // テスト全体で共有する sql.DB 型
@@ -22,13 +30,7 @@ func TestMain(m *testing.M) {
 	teardown()
 }
 
-// 全テスト共通の前処理を書く
-func setup() error {
-	dbUser := "docker"
-	dbPassword := "docker"
-	dbDatabase := "sampledb"
-	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
-
+func connectDB() error {
 	var err error
 	testDB, err = sql.Open("mysql", dbConn)
 	if err != nil {
@@ -37,7 +39,43 @@ func setup() error {
 	return nil
 }
 
+// 全テスト共通の前処理を書く
+func setup() error {
+
+	if err := connectDB(); err != nil {
+		return err
+	}
+	if err := cleanupDB(); err != nil {
+		fmt.Println("cleanup", err)
+		return err
+	}
+	if err := setupTestData(); err != nil {
+		fmt.Println("setup")
+		return err
+	}
+	return nil
+}
+
+func setupTestData() error {
+	cmd := exec.Command("mysql", "-h", "127.0.0.1", "-u", "docker", "sampledb", "--password=docker", "-e", "source ./testdata/setupDB.sql")
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func cleanupDB() error {
+	cmd := exec.Command("mysql", "-h", "127.0.0.1", "-u", "docker", "sampledb", "--password=docker", "-e", "source ./testdata/cleanupDB.sql")
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 // 前テスト共通の後処理を書く
 func teardown() {
+	cleanupDB()
 	testDB.Close()
 }
