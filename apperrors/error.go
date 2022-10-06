b49d2bea9d060a4ee433faf24f3dfbaf33c8b161
ekusuy <ekusuy.omi@gmail.com>
2022-10-06 16:26:11 +0900

    Errフィールドの詳細はjsonエンコード対象から除外する

--- apperrors/error.go
+++ apperrors/error.go
@@ -3,7 +3,7 @@ package apperrors
 type MyAppError struct {
 	ErrCode        // レスポンスとログに表示するエラーコード
 	Message string // レスポンスに表示するエラーメッセージ
-	Err     error
+	Err     error  `json:"-"`
 }
 
 func (myErr *MyAppError) Error() string {
