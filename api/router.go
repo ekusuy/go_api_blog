ba2147abdece9e9e1648f87aa21fe0132360630f
ekusuy <ekusuy.omi@gmail.com>
2022-10-07 01:17:52 +0900

    レスポンスコードを記録するミドルウェア追加

--- api/router.go
+++ api/router.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"database/sql"
+	"github.com/ekusuy/go_api_blog/api/middlewares"
 	"github.com/ekusuy/go_api_blog/controllers"
 	"github.com/ekusuy/go_api_blog/services"
 	"github.com/gorilla/mux"
@@ -20,6 +21,8 @@ func NewRouter(db *sql.DB) *mux.Router {
 	r.HandleFunc("/article/{id:[0-9]+}", aCon.ArticleDetailHandler).Methods(http.MethodGet)
 	r.HandleFunc("/article/nice", aCon.PostNiceHandler).Methods(http.MethodPost)
 	r.HandleFunc("/comment", cCon.PostCommentHandler).Methods(http.MethodPost)
+
+	r.Use(middlewares.LoggingMiddleware)
 
 	return r
 }
